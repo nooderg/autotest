b3f6449541440b9707aa731de76aad141542a2f8
theofenique <theo.fenique@gmail.com>
2022-01-31 13:53:05 +0100

    feat: new file architecture

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,45 @@
-#[macro_use] extern crate rocket;
+#![feature(proc_macro_hygiene, decl_macro)]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+#[macro_use]
+extern crate eventbus;
+#[macro_use]
+extern crate lazy_static;
 
-mod models;
+pub mod application;
+pub mod domain;
+pub mod infrastructure;
+pub mod read_schema;
+pub mod write_schema;
 
-use rocket::serde::json::Json;
-use models::users::User;
+// #[derive(Debug)]
+// struct MyEvent {
+//     i: i32
+// }
 
-#[get("/")]
-fn hello() -> Json<User> {
-    let user = User::create_user();
-    return Json(user)
-}
+// impl Event for MyEvent {}
 
+// fn add_handler(e: &mut MyEvent) {
+//     println!("{:?}", e);
+// }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![hello])
+// let event_bus = EventBus::new();
+// register_hook!(&event_bus, 0, MyEvent, add_handler);
+
+// let mut event = MyEvent { i: 3 };
+
+// post_event!(&event_bus, &mut event, MyEvent);
+
+fn main() {
+    infrastructure::event_bus::register::register_events();
+
+    rocket::ignite()
+        .mount("/users/c/", infrastructure::api::user_routes())
+        .launch();
 }
