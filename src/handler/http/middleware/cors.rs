fd2c1005bb16e2875f9cc7c501d843ef844695ea
theofenique <theo.fenique@gmail.com>
2022-03-09 15:48:14 +0100

    feat: added cors policy

--- src/handler/http/middleware/cors.rs
+++ src/handler/http/middleware/cors.rs
@@ -1,22 +1,22 @@
 use rocket::http::Header;
 use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
+use std::error::Error;
 
-pub struct CORS;
+use rocket::http::Method;
+use rocket::{get, routes};
 
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Attaching CORS headers to responses",
-            kind: Kind::Response
-        }
-    }
 
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+
+pub fn cors() -> rocket_cors::CorsOptions {
+    let allowed_origins = AllowedOrigins::All;
+
+    rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get, Method::Post, Method::Put, Method::Patch, Method::Delete, Method::Options].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::All,
+        allow_credentials: true,
+        ..Default::default()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
+use crate::handler::http::middleware::cors;
+
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
@@ -6,7 +9,7 @@ extern crate rocket;
 extern crate diesel;
 extern crate dotenv;
 
-use crate::handler::http::middleware::cors::CORS;
+
 
 pub mod application;
 pub mod core;
@@ -17,6 +20,6 @@ pub mod handler;
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .attach(CORS)
+        .attach(cors::cors().to_cors().unwrap())
         .mount("/users/", handler::http::user_routes())
 }
